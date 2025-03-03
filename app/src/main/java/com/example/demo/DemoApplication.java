58ef8d338243b874bcc603fd963607fff04cf6cf
Mehmet Alperen Ataner <alperen02070207@gmail.com>
2025-03-03 22:46:00 +0300

    Add files via upload

---
+++ app/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/api/hello")
+    public String hello() throws Exception {
+        String hostname = InetAddress.getLocalHost().getHostName();
+        return "Hello from " + hostname;
+    }
+}
